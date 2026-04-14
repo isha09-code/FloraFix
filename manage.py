486bba39fa176f4412e74a28a80b4105ec99038d
Isha Chakraborty <ishachakraborty@gmail.com>
2026-04-14 17:22:14 +0530

    fix all manage.py conflicts

--- manage.py
+++ manage.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env python
 import os
 import sys
@@ -14,21 +13,4 @@ if __name__ == '__main__':
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-=======
-#!/usr/bin/env python
-import os
-import sys
-
-if __name__ == '__main__':
-    sys.path.insert(0, 'backend')
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kisan_chikitsa.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
->>>>>>> 35e0a5b36eb1bbb2244e40b494c2a14f57220f06
     execute_from_command_line(sys.argv)
