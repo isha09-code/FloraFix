763a19bebaa381ba84c09c9d78b4b035b6d94dfb
Isha Chakraborty <ishachakraborty@gmail.com>
2026-04-14 16:56:11 +0530

    fix build.sh merge conflict

--- build.sh
+++ build.sh
@@ -1,21 +1,6 @@
-<<<<<<< HEAD
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-pip install -r backend/requirements.txt
-
-cd backend
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-=======
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-pip install -r backend/requirements.txt
-
-cd backend
-python manage.py collectstatic --no-input
-python manage.py migrate
->>>>>>> 35e0a5b36eb1bbb2244e40b494c2a14f57220f06
